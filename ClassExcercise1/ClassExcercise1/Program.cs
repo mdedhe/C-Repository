982794216563689faa45484cca187ac200248d18
Mohit Dedhe <mohitdedhe666@gmail.com>
2019-01-22 11:50:02 -0500

    Excercise1Class2

---
+++ ClassExcercise1/ClassExcercise1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassExcercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input the number from user and check whether it is even or odd
+
+            Console.WriteLine("Enter the Number ");
+            string num = Console.ReadLine();
+            Console.WriteLine("Checking whether number is even Or odd ");
+            try
+            {
+                int number = int.Parse(num);
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("Given number is even = " + number);
+                }
+                else
+                {
+                    Console.WriteLine("Given number is odd = " + number);
+                }
+            }
+            catch (NotFiniteNumberException e)
+            {
+                Console.WriteLine("Given character should be the number.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+            finally
+            {
+                Console.WriteLine("Please rerun the program for the next attempt.");
+            }
+        }
+    }
+}

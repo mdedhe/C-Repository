73ab64e23e92caa3a6c464c4916e74fb1531e3a0
Mohit Dedhe <mohitdedhe666@gmail.com>
2019-01-23 12:02:20 -0500

    assingment is completed

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -1,67 +1,123 @@
 ﻿using System;
-
+//getting to know the library from the https://www.donetperls.com/distinct
+using System.Linq;
 namespace Assignment1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /*  Console.WriteLine("Getting the range to print the prime number");
-              int x = 0, y = 10;
-               Console.WriteLine("find the prime numbers between " + x + "and " +y);
-              printPrimeNumber(x, y);
-
-              //getting the series output for the number assigned below:-
-              int n = 2;
-              Console.WriteLine("get the seriesResult for the number = "+n);
-              double result =getSerierResults(n);
-              Console.WriteLine("Output of the getSeriesResult is = " + result);
-              //getting the Binary from decimal number
-              long num = 100;
-              Console.WriteLine("Selected, decimal to convert it to binary number is = " + num);
-              long resultbinary=decimalToBinary(num);
-              Console.WriteLine("Binary conversion of the decimal number " + num + " is = " + resultbinary);
+            Console.WriteLine("Getting the range to print the prime number");
+            int x = 32, y = 100;
+            Console.WriteLine("find the prime numbers between " + x + " and " +y);
+            printPrimeNumber(x, y);
+              
+            //getting the series output for the number assigned below:-
+            int n = 4;
+            Console.WriteLine("");
+            if (n < 0)
+            {
+                Console.WriteLine("Please give the positive integer to get the sum of series.");
+            }
+            else
+            {
+                Console.WriteLine(" get the seriesResult for the number = " + n);
+                double result = getSeriesResult(n);
+                Console.WriteLine("");
+                Console.WriteLine("The sum of the series is: " + result);
+            }
+            //getting the Binary from decimal number
+            long num = 100;
+            if (num < 0)
+            {
+                Console.WriteLine("Kindly use the positive number for conversion.");
+            }
+            else
+            {
+                Console.WriteLine("Selected, decimal to convert it to binary number is = " + num);
+                long resultbinary = decimalToBinary(num);
+                Console.WriteLine("Binary conversion of the decimal number " + num + " is = " + resultbinary);
+            }
+            
             long numb = 1000;
-            long resultdec=binaryTodecimal(numb);
-            Console.WriteLine("Decimal conversion of the binary number " + numb + " is= " + resultdec);
-            int nStar = 5;
-            printTriangle(nStar);*/
-            int[] arr = {1,1,3,4,2,2,2,2,8,8,6};
+            if (numb < 0)
+            {
+                Console.WriteLine("Kindly use the positive number for conversion.");
+            }
+            else
+            {
+                long resultdec = binaryTodecimal(numb);
+                Console.WriteLine("Binary conversion of the decimal number " + numb + " is= " + resultdec);
+            }
+            
+            int nStar = 4;
+            if (nStar <= 1)
+            {
+                Console.WriteLine("Please give a valid integer to print the triangle.");
+            }
+            else
+            {
+                printTriangle(nStar);
+            }
+           
+            int[] arr = {1,1,3,4,2,2,2,2,8,8,9};
             computeFrequency(arr);
         }
         public static void printPrimeNumber(int x, int y) {
             //start the loop between the range
-            for (int i = x; i <= y; i++)
+            try
             {
-                bool isprime = false;
-                for (int j = 1; j < x; j++)
-                {
-                    if (i % j == 0)
-                    {
-                        isprime = true;
-                        break;
-                    }
-                }
-                if (isprime == false)
-                {
-                    Console.Write(i+" ");
-                }
-            }
+                if (x > y)
+                    Console.WriteLine("Please give the valid range for program to run.");
+                else
+                { 
+                    for (int i = x; i <= y; i++)
+                        {
+                             bool ispri = false;
+                             if (i == 0)
+                                continue;
+                            for (int j = 2; j < i; j++)
+                                {
+                                    if (i % j == 0)
+                                        {
+                                               ispri = true;
+                                               break;
+                                        }
+                                }
+                            if (ispri == false)
+                                {
+                                 Console.Write(i + " "); 
+                                }
+                        }//For loop end
+                 }//end of else
+            }//end of try
+            catch
+            {
+                Console.WriteLine("Exception occured while computing printprimeNumbers()");
+            }//end of catch
         }
-        public static double getSerierResults(int n)
+        public static double getSeriesResult(int n)
         {
             // declaring the local variable to store the result
             double finaldec = 0;
-            //initiating loop to create the series as per the requirement
-            for (int i = 1; i <= n; i++) {
-                //getting the factorial for the number which comes first
-               double fact = getFactorial(i);
-                //to create the series making condition statement
-                //1 /2-2!/3+3!/4
-                if (i % 2 != 0)
-                    finaldec += fact / (i + 1);
-                else
-                    finaldec -= fact / (i + 1);
+            try
+            {
+                //initiating loop to create the series as per the requirement
+                for (int i = 1; i <= n; i++)
+                {
+                    //getting the factorial for the number which comes first
+                    double fact = getFactorial(i);
+                    //to create the series making condition statement
+                    //1 /2-2!/3+3!/4
+                    if (i % 2 != 0)
+                        finaldec += fact / (i + 1);
+                    else
+                        finaldec -= fact / (i + 1);
+                }//end of FOR loop
+            }//end of try
+            catch
+            {
+                Console.WriteLine("Execption occurred while computing getSeriesResult()");
             }
             return Math.Round(finaldec, 3);
         }
@@ -78,44 +134,60 @@ namespace Assignment1
         }
         public static long decimalToBinary(long n1)
         {
-            string bin ="";
-            //iterating the number n and divide it to the 2 to get the binary using loop for the same
-            long num = n1;
-            while (num > 0)
+            long binf = 0;
+            try
             {
-                if (num % 2 == 0)
-                    bin +="0";
-                else
-                    bin += "1";
-                num = num / 2;
-            }
-            char[] c = bin.ToCharArray();
-            Array.Reverse(c);
-            string s = new string(c);
-            long binf = long.Parse(s);
+                string bin = "";
+                //iterating the number n and divide it to the 2 to get the binary using loop for the same
+                long num = n1;
+                while (num > 0)
+                {
+                    if (num % 2 == 0)
+                        bin += "0";
+                    else
+                        bin += "1";
+                    num = num / 2;
+                }
+                char[] c = bin.ToCharArray();
+                Array.Reverse(c);
+                string s = new string(c);
+                binf = long.Parse(s);
+            }//end of try
+            catch
+            {
+                Console.WriteLine("Execption occurred while computing decimalToBinary()");
+            }//end of catch
             return binf;
         }
         public static long binaryTodecimal(long n)
         {
             double finaldec = 0;
+            long dec = 0;
             int k = 0;
             string binary = n.ToString();
             char[] c = binary.ToCharArray();
             Array.Reverse(c);
             //check if all the character are zero then decimal is also zero
             bool flag = checkAllzero(c);
-
-            if (flag == false)
+            try
             {
-                foreach (char i in c)
+                if (flag == false)
                 {
-                    string s = char.ToString(i);
-                    int a = int.Parse(s);
-                    finaldec += a* Math.Pow(2, k);
-                    k++;
+                    foreach (char i in c)
+                    {
+                        string s = char.ToString(i);
+                        int a = int.Parse(s);
+                        finaldec += a * Math.Pow(2, k);
+                        k++;
+                    }
                 }
-            }
-            long dec = (long)finaldec;
+                dec = (long)finaldec;
+                
+            }//end of try
+            catch
+            {
+                Console.WriteLine("Execption occurred while computing binaryTodecimal()");
+            }//end of catch
             return dec;
         }
         public static bool checkAllzero(char[] c)
@@ -133,28 +205,62 @@ namespace Assignment1
         }
         public static void printTriangle(int n)
         {
-            for(int i=0; i<5; i++)
+            try
             {
-                for (int j = 0; j < i + 1; j++)
+                int i;
+                for (i = 0; i < n + 1; i++)
                 {
-                    Console.Write("*");
+                    for (int j = n; j > i; j--)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int t = 0; t < (2 *i - 1); t++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
+            }//end of try
+            catch
+            {
+                Console.WriteLine("Exception occured while computing printTriangle()");
             }
         }
         public static void computeFrequency(int[] a)
         {
             int count = 0;
-            Array.Sort(a);
-            for (int i = 0; i < a.Length-1; i++)
+            try
             {
-                count++;
-                if (a[i] != a[i + 1])
-                { 
-                    Console.WriteLine(a[i] + " frequency is = " + count);
+                Array.Sort(a);
+                //1,1,1,3,3,4,5,6,7,7,7,8,8
+                //create the distinct array to do the counting
+
+                int[] adis = a.Distinct().ToArray();
+                Console.Write("Number ");
+                Console.Write("       ");
+                Console.Write("Frequency ");
+                for (int i = 0; i < adis.Length; i++)
+                {
+                    //initialize to 0 for the net iteration
                     count = 0;
+                    for(int j=0; j<a.Length; j++)
+                    {
+                        if (adis[i] == a[j])
+                        {
+                            count++;
+                        }
+
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine(adis[i]+ "       "+count);
+                    
                 }
+            }//end of try
+            catch
+            {
+                Console.WriteLine("Exception occured while computing computeFrequency()");
             }
+
         }
 
     }

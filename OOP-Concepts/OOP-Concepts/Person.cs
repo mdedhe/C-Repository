3b3c16703ec67478bb7d4f0eeef79eb117eeb09a
Mohit Dedhe <mohitdedhe666@gmail.com>
2019-02-05 20:06:13 -0500

    comment

---
+++ OOP-Concepts/OOP-Concepts/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Concepts
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        private string salutation = "Mr.";
+        public string getName()
+        {
+            return salutation+ " "+firstName + " " + lastName;
+        }
+    }
+}
---
+++ OOP-Concepts/OOP-Concepts/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOP_Concepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ps = new Person();
+            ps.firstName = "Mohit";
+            ps.lastName = "Dedhe";
+            Console.WriteLine("My Name is " + ps.getName());
+            Console.ReadKey(true);
+        }
+    }
+}
